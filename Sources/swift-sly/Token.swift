fdfc171580b67a2f78c766d2b5a6df69c18cfa79
Rohit Agarwal <rohit.net@gmail.com>
2024-05-18 13:45:30 -0700

    Added Tokenizable, Token and TokenRegex

--- Sources/swift-sly/Token.swift
+++ Sources/swift-sly/Token.swift
@@ -1,8 +1,45 @@
-//
-//  File.swift
-//  
-//
-//  Created by Rohit Agarwal on 5/18/24.
-//
-
-import Foundation
+/**
+ Representation of a single token, which the lexer recognizes, and parser parses.
+ 
+ The first step of parsing is to break the text into tokens where each token has a type and value.
+ 
+ For example, we have the following text:
+ ```
+ def foo(x, y) x + y * 2
+ ```
+ 
+ The tokens for the above might be described by token types and literals:
+ ```swift
+ public enum TokenTypes: String, Tokenizable {
+     case DEF, ID, NUM
+ }
+ 
+ static var literals = ["(", ")", ",", "+", "*"]
+ ```
+ 
+ And so the tokens identified by the lexer would be list of (name,value) pairs:
+ ```swift
+ [DEF=def, ID=foo, (=(, ID=x, ,,=,, ID=y, )=), ID=x, +=+, ID=y, *=*, NUM=2]
+ ```
+ Token name can be `Tokenizable` or literals. For literals, the name and value
+ of the token is same. 
+ */
+public struct Token<T: Tokenizable>: Hashable {
+    let name: String
+    let value: String
+    
+    init(_ type: T, value: String) {
+        self.name = type.rawValue
+        self.value = value
+    }
+    
+    init(_ type: T) {
+        self.name = type.rawValue
+        self.value = type.rawValue
+    }
+    
+    init(_ literal: String) {
+        self.name = literal
+        self.value = literal
+    }
+}
--- Sources/swift-sly/Tokenizable.swift
+++ Sources/swift-sly/Tokenizable.swift
@@ -1,8 +1,27 @@
-//
-//  File.swift
-//  
-//
-//  Created by Rohit Agarwal on 5/18/24.
-//
-
-import Foundation
+/**
+ Represents token symbols. This is what links the lexer to token to parser.
+ The grammer defines these as terminals, in addititions to literals.
+ 
+ These are generally defined as Enums. E.g.
+ ```swift
+ public enum TokenTypes: String, Tokenizable {
+     case Define, Identifier, Number
+ }
+ ```
+ In this example, the Grammer can use "Define", "Identifier" and "Number" as terminals when defining rules.
+ The casing matters, since the `RawRepresentable` will use the string to match Grammer symbols.
+ 
+ The raw values can be different, but they have to match symbols used in the Grammer.
+ ```swift
+ public enum TokenTypes: String, Tokenizable {
+     case Define = "DEF"
+     case Identifier = "ID"
+     case Number = "NUM"
+ }
+ ```
+ Here the Grammer can use "DEF", "ID" and "NUM" as terminals when defining rules.
+ */
+public protocol Tokenizable:
+    RawRepresentable,
+    Hashable
+    where RawValue == String {}
